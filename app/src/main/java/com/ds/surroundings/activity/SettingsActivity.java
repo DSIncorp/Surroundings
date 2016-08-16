c2a49cae79c58c48dc39882ace109544c8cfae49
Oleksandr Rak <zanderrak@gmail.com>
2016-08-16 09:18:08 +0300

    Create SettingsActivity class and activity_settings.xml

---
+++ app/src/main/java/com/ds/surroundings/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.ds.surroundings.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ds.surroundings.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
