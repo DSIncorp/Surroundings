4c02c599795b74461b15d55ddb75974cebcf0ba5
Oleksandr Rak <zanderrak@gmail.com>
2016-08-16 12:42:29 +0300

    Create place bean

---
+++ app/src/main/java/com/ds/surroundings/bean/Place.java
@@ -0,0 +1,17 @@
+package com.ds.surroundings.bean;
+
+import java.util.List;
+
+/**
+ * Created by Oleksandr Rak on 16.08.2016.
+ */
+public class Place {
+    private String id;
+    private String scope;
+    private String name;
+    private double latitude;
+    private double length;
+    private String icon;
+    private List<String> types;
+    private String vicinity;
+}
